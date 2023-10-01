14800ad7c4808a55b664e6aab5ef2c24223becde
Nick Fletcher <nfletcher@canibuild.com>
2023-10-01 18:24:06 +1100

    Add content

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const copyDate = new Date().getFullYear();
+document.getElementById("copy").innerHTML = "&copy; Safety Pin Design " + copyDate;
+
+// window.fbAsyncInit = function() {
+//   FB.init({
+//     appId            : 'your-app-id',
+//     xfbml            : true,
+//     version          : 'v18.0'
+//   });
+// };
+
+// use this to auto load images, process into a grid: 
